28c9301145ddfb8863e9a390b371cceecd1efc17
ranlolxp <ranlolxp@gmail.com>
2026-08-19 22:59:18 -0700

    Actualizar PDF del menu

--- js/i18n.js
+++ js/i18n.js
@@ -10,8 +10,8 @@ var CONTACT = {
   instagram: 'meyra.cafe.restaurante.lpz',
   instagramUrl: 'https://www.instagram.com/meyra.cafe.restaurante.lpz/',
   mapsUrl: 'https://www.google.com/maps/search/MEYRA+Caf%C3%A9+y+Restaurante/@24.1475197,-110.3343323,17z',
-  menuPdfEs: 'assets/menu/menu-meyra-es.pdf?v=20260626',
-  menuPdfEn: 'assets/menu/menu-meyra-en.pdf?v=20260626',
+  menuPdfEs: 'assets/menu/menu-meyra-es.pdf?v=20260819',
+  menuPdfEn: 'assets/menu/menu-meyra-en.pdf?v=20260819',
 };
 
 var SHAMRAM_CONTACT = {
